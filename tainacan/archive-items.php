cd103c8a5932a282798d57cc404dbb74443f501f
mateuswetah <mateus.m.luna@gmail.com>
2020-06-25 09:43:18 -0300

    Adds title bar and other wrappers to tainacan item list pages.

--- tainacan/archive-items.php
+++ tainacan/archive-items.php
@@ -1,5 +1,62 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all pages.
+ */
 
-	<?php tainacan_the_faceted_search(); ?>
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
 
-<?php get_footer();
+if(!apollo13framework_check_for_renamed_templates()){
+	//we are moving to different template
+	return;
+}
+
+
+if(post_password_required()){
+	/* don't use the_content() as it also applies filters that we don't need here, if we are using custom password page */
+	echo get_the_content();
+}
+else{
+	global $apollo13framework_a13;
+	get_header();
+
+	// Elementor `single` location
+	if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ){
+
+		the_post();
+		apollo13framework_title_bar();
+		$add_class       = 'content-box';
+		$sticky_one_page = $apollo13framework_a13->get_meta( '_content_sticky_one_page' ) === 'on';
+		if( $sticky_one_page ){
+			$add_class .= ' a13-sticky-one-page';
+		}
+		?>
+
+		<article id="content" class="clearfix"<?php apollo13framework_schema_args('creative'); ?>>
+			<div class="content-limiter">
+				<div id="col-mask">
+
+					<div id="post-<?php the_ID(); ?>" <?php
+					post_class( $add_class );
+					if( $sticky_one_page ){
+						echo ' data-a13-sticky-one-page-icon-global-color="' . esc_attr( $apollo13framework_a13->get_meta( '_content_sticky_one_page_bullet_color' ) ) . '"';
+						echo ' data-a13-sticky-one-page-icon-global-icon="' . esc_attr( $apollo13framework_a13->get_meta( '_content_sticky_one_page_bullet_icon' ) ) . '"';
+					}
+					?>>
+					<?php apollo13framework_title_bar( 'inside' ); ?>
+					<div class="real-content" <?php apollo13framework_schema_args('text'); ?>></div>
+						<?php tainacan_the_faceted_search(); ?>
+					</div>
+
+					</div>
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</article>
+
+		<?php
+	}
+	get_footer();
+}//end of if password_protected
+
